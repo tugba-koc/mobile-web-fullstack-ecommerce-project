3a9fc4076c68790247194dda33c6f0bddad09220
tugba-koc <koctug@itu.edu.tr>
2026-03-26 00:40:40 +0300

    fix: update .gitignore to include environment files and modify ClerkProvider to use publishableKey

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/react'
 
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider>
+    <ClerkProvider publishableKey={publishableKey}>
       <App />
     </ClerkProvider>
   </StrictMode>,
