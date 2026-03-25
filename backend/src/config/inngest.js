992561bb5af34b28fd8f9cbdd69d2a6086544a6b
tugba-koc <koctug@itu.edu.tr>
2026-03-26 00:06:15 +0300

    fix: update build script in package.json and correct Inngest function triggers

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -7,8 +7,7 @@ export const inngest = new Inngest({
 });
 
 const syncUser = inngest.createFunction(
-  { id: 'sync-user' },
-  { event: 'clerk/user.created' },
+  { id: 'sync-user', triggers: [{ event: 'clerk/user.created' }] },
   async ({ event }) => {
     await connectDB();
     const { id, email_addresses, first_name, last_name, image_url } =
@@ -28,8 +27,7 @@ const syncUser = inngest.createFunction(
 );
 
 const deleteUserFromDB = inngest.createFunction(
-  { id: 'delete-user-from-db' },
-  { event: 'clerk/user.deleted' },
+  { id: 'delete-user-from-db', triggers: [{ event: 'clerk/user.deleted' }] },
   async ({ event }) => {
     await connectDB();
     const { id } = event.data;
