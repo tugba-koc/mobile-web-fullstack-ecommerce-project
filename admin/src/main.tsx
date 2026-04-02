90b0a9f6b157913f231bd06182231adcae8717da
tugba-koc <koctug@itu.edu.tr>
2026-04-02 23:54:07 +0300

    update main.tsx

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/react'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
   </StrictMode>,
